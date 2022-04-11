f25caacf61b3d8b152ea78285ac85897c739c2b7
shimadatuyoshi <k020g1186@g.neec.ac.jp>
2022-04-11 14:18:06 +0900

    fbxここからスタート

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include<d3dx12.h>
 #include"MyGame.h"
 #include"Framework.h"
+#include"fbxsdk.h"
 
 #pragma comment(lib,"dinput8.lib")
 #pragma comment(lib,"dxguid.lib")
@@ -23,6 +24,8 @@
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+
+	FbxManager* fbxManager = FbxManager::Create();
 	Framework* game=new MyGame;
 
 	game->Run();
